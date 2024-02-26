827ed094ef7d3f95f46d95d7f68e73d59e575b14
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2024-02-27 01:35:29 +0200

    add use useRef

--- webSockets/web-sockets-react-basis/src/App.jsx
+++ webSockets/web-sockets-react-basis/src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import Title from './components/Title/Title';
 
 function App() {
-  const [ws, setWs] = useState(null);
+  const ws = useRef(new WebSocket('ws://localhost:8080/'));
   const [error, setEror] = useState(null);
   const [btnName, setBtnName] = useState('Test Button');
   useEffect(() => {
@@ -12,21 +12,19 @@ function App() {
   }, []);
 
   function connection() {
-    const ws = new WebSocket('ws://localhost:8080/');
-    ws.onopen = () => {
+    ws.current.onopen = () => {
       console.log('Connection Established!');
-      setWs(ws);
     };
-    ws.onmessage = (e) => {
+    ws.current.onmessage = (e) => {
       setBtnName(e.data);
     };
-    ws.onerror = () => {
+    ws.current.onerror = () => {
       setEror('Error, try again later! ');
     };
   }
   const handleClick = () => {
     const name = prompt('set new button name');
-    ws.send(name);
+    ws.current.send(name);
   };
   return <>{error ? <Title title={error} /> : <Button click={handleClick}>{btnName}</Button>}</>;
 }
